5286ddbef851bdd5670bce84c9b9d3db97420a35
Iván Serrano <contacto.ivanserrano@gmail.com>
2023-07-17 14:19:09 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage:{
+        "sanFrancisco":"url('../img/sanFrancisco.jpg')",
+        "sanFranciscoDesktop":"url('../img/sanFranciscoDesktop.jpg')",
+        "yosemite":"url('../img/yosemite.jpg')",
+        "LA":"url('../img/LA.jpg')",
+        "seattle":"url('../img/seattle.jpg')",
+        "new_york":"url('../img/new_york.jpg')",
+        "norway":"url('../img/norway.jpg')",
+        "sydney":"url('../img/sydney.jpg')",
+        "miami":"url('../img/miami.jpg')",
+        "switzerland":"url('../img/switzerland.jpg')",
+        "bali":"url('../img/bali.jpg')",
+        "chicago":"url('../img/chicago.jpg')",
+        "europe":"url('../img/europe.jpg')",
+        "iceland":"url('../img/iceland.jpg')",
+      },
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#26A79B',
+        'terciary': '#61AEC9'
+      }),
+      textColor:{
+        'primary': '#CC2D4A',
+        'secondary': '#26A79B',
+        'terciary': '#61AEC9'
+      },
+      fontFamily: { 
+        Montserrat: ['Monserrat', 'sans-serif']
+      },
+      variants: {
+        width: ["responsive", "hover", "focus"],
+        extend: {},
+      },
+    },
+  },
+  plugins: [],
+};
